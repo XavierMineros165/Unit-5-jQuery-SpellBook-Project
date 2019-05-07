37e987145ef65068eaefb3e1fffc013fe3e827a1
XavierMineros165 <47435059+XavierMineros165@users.noreply.github.com>
2019-05-07 14:51:23 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".mago").click(function() {
+    $(".conejo").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".mago").click(function(){
+    $("#perro").toggle();
+});
+
+
+
+//Spell #3
+
+$(".mago").click(function(){
+    $("#gato").slideUp();
+});
+
+
+
